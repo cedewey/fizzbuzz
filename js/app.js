59a7faa04655b59ae97cb44ff21fe88d479d5b24
Clayton Dewey <claytondewey@gmail.com>
2015-03-01 12:53:16 -0600

    Updates this for FizzBuzz Refactor

--- js/app.js
+++ js/app.js
@@ -1,16 +1,28 @@
 $(document).ready(function() {
-	for (var num = 1; num <= 100; num++) {
-		if (num % 15 == 0) {
-			$('body').append('fizzbuzz</br>');
+	var max = prompt('Enter a number my friend.');
+	if (isNaN(max)) {
+		alert("I said NUMBER. $ಠ_ಠ");
+		var max = prompt('Enter a number my friend.');
+	}
+	var max = +max;
+
+
+
+function fizzBuzz(max) {
+		for (var num = 1; num <= max; num++) {
+			if (num % 15 == 0) {
+				$('body').append('fizzbuzz</br>');
+			}
+			else if (num % 3 == 0) {
+				$('body').append('fizz</br>');
+			}
+			else if (num % 5 == 0) {
+				$('body').append('buzz</br>');
+			}
+			else {
+				$('body').append(num + '</br>');
+			}
 		}
-		else if (num % 3 == 0) {
-			$('body').append('fizz</br>');
-		}
-		else if (num % 5 == 0) {
-			$('body').append('buzz</br>');
-		}
-		else {
-			$('body').append(num + '</br>');
-		}
-	};
+}
+	fizzBuzz(max);
 });
